834474b1112ecca6a9a2fed4ed4b131273255e19
Dargons10 <jdamr2p@gmail.com>
2020-10-03 21:04:43 +0200

    Added macadrr for bluettoth

--- proprietary/bin/macaddr.sh
+++ proprietary/bin/macaddr.sh
@@ -1,8 +1,6 @@
 #!/system/bin/sh
 
 TAG="mac_generator"
-WIFI_FILE="/data/mocha_macaddr.txt"
-BT_FILE="/data/mocha_btmacaddr.txt"
 
 bkbIsBroken=0
 #for Android
@@ -13,19 +11,18 @@ md5serialno=$(echo -n $Serialno | md5sum)
 
 checkBkbPartition() {
 	echo "$TAG: attempt to read BKB partition"
-	local BKB=$(cat /dev/block/platform/sdhci-tegra.3/by-name/BKB | grep -a XIAOMI)
-	if [ "$BKB" = "" ]; then
+	#local BKB=$(cat /dev/block/platform/sdhci-tegra.3/by-name/BKB | grep -a XIAOMI)
+	#if [ "$BKB" = "" ]; then
 		bkbIsBroken=1
-	fi;
-}
+	#fi;
+} 
 
 generateBtMac() {
 	echo "$TAG: generating bt mac address"
 	local btMac="${md5serialno:3:2}:${md5serialno:16:2}:${md5serialno:17:2}:${md5serialno:9:2}:${md5serialno:11:2}:${md5serialno:13:2}"
-	echo $btMac > ${BT_FILE}
-	chown bluetooth:bluetooth ${BT_FILE}
-	chmod 644 ${BT_FILE}
-	setprop ro.bt.bdaddr_path ${BT_FILE}
+	echo $btMac > /vendor/etc/mocha_btmacaddr.txt
+	chown bluetooth:bluetooth /vendor/etc/mocha_btmacaddr.txt
+	setprop ro.bt.bdaddr_path /vendor/etc/mocha_btmacaddr.txt
 	setprop persist.service.bdroid.bdaddr $btMac
 	setprop ro.boot.btmacaddr $btMac
 }
@@ -33,27 +30,18 @@ generateBtMac() {
 generateWifiMac() {
 	echo "$TAG: generating wifi mac address"
 	local wifiMac="0c:1d:${md5serialno:7:2}:${md5serialno:9:2}:${md5serialno:11:2}:${md5serialno:14:2}"
-	echo $wifiMac > ${WIFI_FILE}
-	chmod 644 ${WIFI_FILE}
+	echo $wifiMac > /vendor/etc/mocha_macaddr.txt
+	chmod 644 /vendor/etc/mocha_macaddr.txt
 }
 
 main() {
-	if [ -f ${BT_FILE} ] && [ -f ${WIFI_FILE} ]; then
-		echo "$TAG: mac address file exist"
-		btMac=`cat ${BT_FILE}`
-		setprop ro.bt.bdaddr_path ${BT_FILE}
-		setprop persist.service.bdroid.bdaddr $btMac
-		setprop ro.boot.btmacaddr $btMac
-		chown bluetooth:bluetooth ${BT_FILE}
-		chmod 644 ${BT_FILE}
-		chmod 644 ${WIFI_FILE}
-		exit
-	fi
 	checkBkbPartition
 
 	if [ "$bkbIsBroken" = 1 ]; then
+		mount -o remount,rw /system
 		generateBtMac
 		generateWifiMac
+		mount -o remount,ro /system
 	else
 		echo "$TAG: BKB partion is not broken"
 	fi;
